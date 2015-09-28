ad728d04114d71a6f0eb7261fab4695b6cd564ed
Bayle38 <stephane.bayle38@gmail.com>
2015-09-28 08:16:53 +0200

    Initialisation du projet

---
+++ src/jvn/JvnCoordImpl.java
@@ -0,0 +1,111 @@
+/***
+ * JAVANAISE Implementation
+ * JvnServerImpl class
+ * Contact: 
+ *
+ * Authors: 
+ */
+
+package jvn;
+
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.io.Serializable;
+
+
+public class JvnCoordImpl 	
+              extends UnicastRemoteObject 
+							implements JvnRemoteCoord{
+
+	/**
+	 * TODO: Créer un tableau contenant les jvn objects, leurs id, leurs noms, et la liste des serveurs qui ont besoin d'etre informés de la modification de la ressource.
+	 * 
+	 */
+	private JvnObject dernier;
+	private HashMap<String, JvnObject> table;
+
+  /**
+  * Default constructor
+  * @throws JvnException
+  **/
+	private JvnCoordImpl() throws Exception {
+		table = new HashMap<String, JvnObject>();
+		dernier=null;
+		// to be completed
+	}
+
+  /**
+  *  Allocate a NEW JVN object id (usually allocated to a 
+  *  newly created JVN object)
+  * @throws java.rmi.RemoteException,JvnException
+  **/
+  public int jvnGetObjectId()
+  throws java.rmi.RemoteException,jvn.JvnException {
+    // to be completed 
+    return dernier.jvnGetObjectId();
+  }
+  
+  /**
+  * Associate a symbolic name with a JVN object
+  * @param jon : the JVN object name
+  * @param jo  : the JVN object 
+  * @param joi : the JVN object identification
+  * @param js  : the remote reference of the JVNServer
+  * @throws java.rmi.RemoteException,JvnException
+  **/
+  public void jvnRegisterObject(String jon, JvnObject jo, JvnRemoteServer js)
+  throws java.rmi.RemoteException,jvn.JvnException{
+    // to be completed
+	table.put(jon, jo);
+  }
+  
+  /**
+  * Get the reference of a JVN object managed by a given JVN server 
+  * @param jon : the JVN object name
+  * @param js : the remote reference of the JVNServer
+  * @throws java.rmi.RemoteException,JvnException
+  **/
+  public JvnObject jvnLookupObject(String jon, JvnRemoteServer js)
+  throws java.rmi.RemoteException,jvn.JvnException{
+    // to be completed 
+    return null;
+  }
+  
+  /**
+  * Get a Read lock on a JVN object managed by a given JVN server 
+  * @param joi : the JVN object identification
+  * @param js  : the remote reference of the server
+  * @return the current JVN object state
+  * @throws java.rmi.RemoteException, JvnException
+  **/
+   public Serializable jvnLockRead(int joi, JvnRemoteServer js)
+   throws java.rmi.RemoteException, JvnException{
+    // to be completed
+    return null;
+   }
+
+  /**
+  * Get a Write lock on a JVN object managed by a given JVN server 
+  * @param joi : the JVN object identification
+  * @param js  : the remote reference of the server
+  * @return the current JVN object state
+  * @throws java.rmi.RemoteException, JvnException
+  **/
+   public Serializable jvnLockWrite(int joi, JvnRemoteServer js)
+   throws java.rmi.RemoteException, JvnException{
+    // to be completed
+    return null;
+   }
+
+	/**
+	* A JVN server terminates
+	* @param js  : the remote reference of the server
+	* @throws java.rmi.RemoteException, JvnException
+	**/
+    public void jvnTerminate(JvnRemoteServer js)
+	 throws java.rmi.RemoteException, JvnException {
+	 // to be completed
+    }
+}
+
+ 
