f20e2aa2d8b1e121702336590407ed4ae1b15b69
Bayle38 <stephane.bayle38@gmail.com>
2015-09-28 10:26:18 +0200

    Créaton du JvnObjectImpl

---
+++ src/jvn/JvnObjectImpl.java
@@ -0,0 +1,49 @@
+package jvn;
+
+import java.io.Serializable;
+
+public class JvnObjectImpl implements JvnObject {
+
+	Serializable obj;
+	public JvnObjectImpl(Serializable o) {
+		obj = o;
+	}
+
+	public void jvnLockRead() throws JvnException {
+		// TODO Auto-generated method stub
+	}
+
+	public void jvnLockWrite() throws JvnException {
+		// TODO Auto-generated method stub
+	}
+
+	public void jvnUnLock() throws JvnException {
+		// TODO Auto-generated method stub
+	}
+
+	public int jvnGetObjectId() throws JvnException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Serializable jvnGetObjectState() throws JvnException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void jvnInvalidateReader() throws JvnException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Serializable jvnInvalidateWriter() throws JvnException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Serializable jvnInvalidateWriterForReader() throws JvnException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
