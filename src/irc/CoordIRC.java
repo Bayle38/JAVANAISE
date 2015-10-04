24fbf2f881bdf92f7ff1f64de6ff63f1d163f0c8
Bayle38 <stephane.bayle38@gmail.com>
2015-10-04 22:30:59 +0200

    Lanceur du coordinateur

---
+++ src/irc/CoordIRC.java
@@ -0,0 +1,17 @@
+package irc;
+
+import jvn.JvnCoordImpl;
+
+public class CoordIRC {
+
+	
+	public static void main (String args[]){
+		try {
+			JvnCoordImpl co = new JvnCoordImpl();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
